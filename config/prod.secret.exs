ca2b327cee40139d4c666e8ec465c09a0b90eb2b
Vinicius <35617139+shiryel@users.noreply.github.com>
2020-06-24 04:43:32 -0300

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -35,7 +35,7 @@ config :shiryel_blog, ShiryelBlogWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :shiryel_blog, ShiryelBlogWeb.Endpoint, server: true
+config :shiryel_blog, ShiryelBlogWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
