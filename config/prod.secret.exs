99999b389d13699da5504f119bb34b425473f6e3
Vinicius <35617139+shiryel@users.noreply.github.com>
2020-06-24 04:49:12 -0300

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,10 +11,10 @@ use Mix.Config
 #    For example: ecto://USER:PASS@HOST/DATABASE
 #    """
 
-config :shiryel_blog, ShiryelBlog.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+#config :shiryel_blog, ShiryelBlog.Repo,
+#  # ssl: true,
+#  url: database_url,
+#  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
