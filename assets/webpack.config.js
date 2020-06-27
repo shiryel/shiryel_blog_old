8dfe1a1903f1c34105ac409f981c887609ac7ac9
shiryel <shiryel@hotmail.com>
2020-06-26 23:04:10 -0300

    Remove split chunks

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -22,25 +22,7 @@ module.exports = (env, options) => {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
         new OptimizeCSSAssetsPlugin({})
-      ],
-      splitChunks: {
-      chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              // get the name. E.g. node_modules/packageName/not/this/part.js
-              // or node_modules/packageName
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `npm.${packageName.replace('@', '')}`;
-            },
-          },
-        },
-      },
+      ]
     },
     entry: {
       'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
